d4c59c2d97c56b020e9df794a8ff75144352a1a6
Sam Neirinck <github@samneirinck.be>
2014-08-07 17:34:49 +0200

    Add missing files

---
+++ SonosSharp/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Linq;
+namespace SonosSharp
+{
+    public static class Constants
+    {
+        public const int SonosPortNumber = 1400;
+
+
+        public static XNamespace UpnpNamespace = @"urn:schemas-upnp-org:device-1-0";
+        public static XNamespace SoapNamespace = @"http://schemas.xmlsoap.org/soap/envelope/";
+        public static XNamespace RenderingControl = @"urn:schemas-upnp-org:service:RenderingControl:1";
+
+    }
+}
