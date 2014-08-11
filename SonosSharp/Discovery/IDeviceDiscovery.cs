d911415ac2c309816f8c45ffec07a2e205280c27
Sam Neirinck <github@samneirinck.be>
2014-08-11 20:35:19 +0200

    Add WP8

--- SonosSharp/Discovery/IDeviceDiscovery.cs
+++ SonosSharp/Discovery/IDeviceDiscovery.cs
@@ -1,6 +1,9 @@
-﻿namespace SonosSharp.Discovery
+﻿using System;
+using System.Threading.Tasks;
+namespace SonosSharp.Discovery
 {
     public interface IDeviceDiscovery
     {
+        Task SearchForSonosDevicesAsync(Action<string> deviceFound);
     }
 }
