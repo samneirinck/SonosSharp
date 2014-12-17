f31b82bda9e1826bee9267ea068d858c2d004504
Sam Neirinck <sam@samneirinck.be>
2014-12-17 20:34:12 +0100

    Add GlobalAssemblyInfo

--- src/SonosSharp.WP8/Properties/AssemblyInfo.cs
+++ src/SonosSharp.WP8/Properties/AssemblyInfo.cs
@@ -8,30 +8,6 @@ using System.Resources;
 // associated with an assembly.
 [assembly: AssemblyTitle("SonosSharp.WP8")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SonosSharp.WP8")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d6e32a11-c045-4a0f-80a2-fb5704115571")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: NeutralResourcesLanguageAttribute("en-US")]
