acc1c34ce39b611e05c57f9d35c1fd7541298dcb
Sam Neirinck <sam@samneirinck.com>
2016-09-01 09:51:17 +0200

    Set up project structure

---
+++ src/SonosSharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("SonosSharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SonosSharp")]
+[assembly: AssemblyCopyright("Copyright ©  116")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("4FF51E0D-D4F1-41BA-B7A3-9323EF1F6DB8")]
+
---
+++ test/SonosSharp.Tests/Services/DeviceDiscovery.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+using SonosSharp.Services;
+
+namespace SonosSharp.UnitTests
+{
+    public class DeviceDiscovery_DiscoverDevicesAsyncShould
+    {
+        private readonly DeviceDiscovery _service;
+
+        public DeviceDiscovery_DiscoverDevicesAsyncShould()
+        {
+            _service = new DeviceDiscovery();
+        }
+
+        [Fact]
+        public async Task DiscoverDevices() 
+        {
+            var result = await _service.DiscoverDevicesAsync();
+
+            Assert.NotNull(result);
+        }
+    }
+}
